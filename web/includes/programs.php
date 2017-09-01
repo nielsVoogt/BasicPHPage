589205cd197a6aea1156c6a59d2370390fc27f8a
nielsVoogt <niels@branchonline.nl>
2017-09-01 18:47:00 +0200

    Minor edit for program include

--- web/includes/programs.php
+++ web/includes/programs.php
@@ -1,3 +1,24 @@
+<?php
+$programs = array(
+    array(
+        "programName" => "asjdhdasj1",
+        "programDesc" => "nsequat auctor eu in elit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Mauris in erat justo. Nullam ac urna eu felis dapibus condimentum sit amet a augue. Sed non neque elit. Sed ut imperdiet nisi.
+            Proin condimentum fermentum nunc. Etiam dsa pharetra, erat sed fermentum feugiat, velit dfas mauris egestas quam, ut aliquam massa nisl quis neque. Suspendisse in orci enim.",
+        "programUrl" => ".adssadsadsad",
+    ),
+    array(
+        "programName" => "asjdhdasj123",
+        "programDesc" => "nseqfgfff333333uat auctor eu in elit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Mauris in erat justo. Nullam ac urna eu felis dapibus condimentum sit amet a augue. Sed non neque elit. Sed ut imperdiet nisi.
+            Proin condimentum fermentum nunc. Etiam dsa pharetra, erat sed fermentum feugiat, velit dfas mauris egestas quam, ut aliquam massa nisl quis neque. Suspendisse in orci enim.",
+        "programUrl" => ".adssadsadsad",
+    ),
+    array(
+        "programName" => "asjdhdasj2",
+        "programDesc" => "asdjasdkds dasdasa",
+        "programUrl" => ".adsadsdasadsasdd",
+    ));
+?>
+
 <?php for ($i = 0; $i < count($programs); $i++): ?>
     <div class='yolo'>
         <p><?= $programs[$i]["programName"] ?></p>
--- web/index.php
+++ web/index.php
@@ -10,21 +10,6 @@
     <body>
         <?php include('includes/browserwarning.php'); ?>
         <?php include('includes/navigation.php'); ?>
-        
-        <?php
-        $programs = array(
-            array(
-                "programName" => "asjdhdasj1",
-                "programDesc" => "nsequat auctor eu in elit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Mauris in erat justo. Nullam ac urna eu felis dapibus condimentum sit amet a augue. Sed non neque elit. Sed ut imperdiet nisi.
-                    Proin condimentum fermentum nunc. Etiam dsa pharetra, erat sed fermentum feugiat, velit dfas mauris egestas quam, ut aliquam massa nisl quis neque. Suspendisse in orci enim.",
-                "programUrl" => ".adssadsadsad",
-            ),
-            array(
-                "programName" => "asjdhdasj2",
-                "programDesc" => "asdjasdkds dasdasa",
-                "programUrl" => ".adsadsdasadsasdd",
-            ));
-        ?>
         <?php include('includes/programs.php'); ?>
         <?php include('includes/footer.php'); ?>
     </body>

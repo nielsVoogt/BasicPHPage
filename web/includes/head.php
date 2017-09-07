89c0b59686ed195faa09ff067c667e0910fb226a
nielsVoogt <niels@branchonline.nl>
2017-09-07 13:26:41 +0200

    Removing the modernizr css

--- web/includes/head.php
+++ web/includes/head.php
@@ -4,7 +4,6 @@
     <title><?php echo $title; ?></title>
     <meta name="description" content="<?php echo $description; ?>" >
 
-    <link rel="stylesheet" href="css/normalize.min.css">
     <link rel="stylesheet" href="css/main.css">
 
     <meta name="viewport" content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0'>
